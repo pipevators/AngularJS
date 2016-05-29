de512226f40baf8ee9ba48611553d81d703e07bd
Felipe Rodriguez R <felipe.rodriguez@solem.cl>
2016-05-28 23:51:29 -0300

    Desafio 3
    Signed-off-by: Felipe Rodriguez R <felipe.rodriguez@solem.cl>

--- Desafio3/js/desafio.js
+++ Desafio3/js/desafio.js
@@ -1,5 +1,5 @@
 
-angular.module("desafio",[])
+angular.module("desafio",['ngMessages'])
 	.controller("formController",function(){
 
 		var vCtrl = this,
@@ -9,14 +9,21 @@ angular.module("desafio",[])
             init = function () {
                 //Visivilidad de boton confirmar
                 vCtrl.confirmButton = true;
+                vCtrl.regionList = regiones;
+                vCtrl.communeList = comunas;
                 vCtrl.inputData = {};
                 vCtrl.showData = {};
+                vCtrl.region = "";
+                vCtrl.commune = "";
             };
-
-		
 
 		//Submit de formulario
 		vCtrl.submit = function() {
+
+			vCtrl.inputData.commune = vCtrl.commune.code;
+			vCtrl.inputData.region = vCtrl.region.code;
+			//Concatena comuna + region en direccion 2
+			vCtrl.inputData.dir2 = vCtrl.commune.name +', ' + vCtrl.region.name;
 
 			vCtrl.showData = angular.copy(vCtrl.inputData);
             
@@ -34,10 +41,11 @@ angular.module("desafio",[])
 		//Limpia variables
 		vCtrl.clean = function() {
             init();
-            
-            // retorna true para que el flujo del evento continúe normalmente (está asociado 
-            // a un botón de tipo reset)
             return true;
+		};
+
+		vCtrl.selectedRegion = function(data){
+			console.log("DATA:",data);
 		};
 
 		init();
---
+++ Desafio3/js/regiones.js
@@ -0,0 +1,78 @@
+//JSON con regiones de Chile
+var regiones = [
+	{
+        "name": "Arica y Parinacota",
+        "code": 15,
+        "region_number": "XV"
+    },
+    {
+        "name": "Tarapacá",
+        "code": 1,
+        "region_number": "I"
+    },
+    {
+        "name": "Antofagasta",
+        "code": 2,
+        "region_number": "II"
+    },
+    {
+        "name": "Atacama",
+        "code": 3,
+        "region_number": "III"
+    },
+    {
+        "name": "Coquimbo",
+        "code": 4,
+        "region_number": "IV"
+    },
+    {
+        "name": "Valparaíso",
+        "code": 5,
+        "region_number": "V"
+    },
+    {
+        "name": "Región Metropolitana",
+        "code": 13,
+        "region_number": "XIII"
+    },
+    {
+        "name": "O'Higgins",
+        "code": 6,
+        "region_number": "VI"
+    },
+    {
+        "name": "Maule",
+        "code": 7,
+        "region_number": "VII"
+    },
+    {
+        "name": "Biobío",
+        "code": 8,
+        "region_number": "VIII"
+    },
+    {
+        "name": "La Araucanía",
+        "code": 9,
+        "region_number": "IX"
+    },
+    {
+        "name": "Los Ríos",
+        "code": 14,
+        "region_number": "XIV"
+    },
+    {
+        "name": "Los Lagos",
+        "code": 10,
+        "region_number": "X"
+    },
+    {
+        "name": "Aysén",
+        "code": 11,
+        "region_number": "XI"
+    },
+    {
+        "name": "Magallanes y Antártica",
+        "code": 12,
+        "region_number": "XII"
+    }
+];
